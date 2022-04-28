7b18b139789de7dcc2abde5693e2420257a83ce1
saamuhymin <102709114+saamuhymin@users.noreply.github.com>
2022-04-28 17:56:39 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -4,61 +4,13 @@ import numpy as np
 import pandas as pd
 import time
 
-st.header("My first Python Web App")
+st.header("Your Personal Body Mass Index (BMI) Instructor")
 
-readme = st.checkbox("readme first")
+readme = st.checkbox("What is BMI?")
 
 if readme:
 
     st.write("""
-        This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [heroku](https://www.heroku.com/). You may get the codes via [github](https://github.com/richieyuyongpoh/myfirstapp)
-        """)
-
-    st.write ("For more info, please contact:")
-
-    st.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
-
-option = st.sidebar.selectbox(
-    'Select a mini project',
-     ['line chart','map','T n C','Long Process'])
-
-
-if option=='line chart':
-    chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-    st.line_chart(chart_data)
-
-elif option=='map':
-    map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-    st.map(map_data)
-
-elif option=='T n C':
-
-    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-    show = st.checkbox('I agree the terms and conditions')
-    if show:
-        st.write(pd.DataFrame({
-        'Intplan': ['yes', 'yes', 'yes', 'no'],
-        'Churn Status': [0, 0, 0, 1]
-        }))
-
-
-else:
-    'Starting a long computation...'
-
-    
-    latest_iteration = st.empty()
-    bar = st.progress(0)
-
-    for i in range(100):
-   
-        latest_iteration.text(f'Iteration {i+1}')
-        bar.progress(i + 1)
-        time.sleep(0.1)
-
-    '...and now we\'re done!'
+        Body mass index. A measure that relates body weight to height. BMI is sometimes used to measure total body fat and whether a person is a healthy weight. Excess body fat is linked to an increased risk of some diseases including heart disease and some cancers.
+        According to <a href='https://en.wikipedia.org/wiki/Body_mass_index/'>Wikipedia </a>, Body mass index is a value derived from the mass and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m², resulting from mass in kilograms and height in metres.
+        """, unsafe_allow_html=True)
