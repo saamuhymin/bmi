ba5472286e1aa36cb194cb901368bd382e09ceb2
saamuhymin <102709114+saamuhymin@users.noreply.github.com>
2022-04-30 04:50:18 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -12,7 +12,7 @@ if readme:
 
     st.write("""
         Body mass index. A measure that relates body weight to height. BMI is sometimes used to measure total body fat and whether a person is a healthy weight. Excess body fat is linked to an increased risk of some diseases including heart disease and some cancers.
-        According to <a href='https://en.wikipedia.org/wiki/Body_mass_index/'>Wikipedia </a>, Body mass index is a value derived from the mass and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m², resulting from mass in kilograms and height in metres.
+        According to <a href='https://en.wikipedia.org/wiki/Body_mass_index/'>Wikipedia</a>, Body mass index is a value derived from the mass and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m², resulting from mass in kilograms and height in metres.
         """, unsafe_allow_html=True)
 
     ###if(st.button("About")):
@@ -31,23 +31,23 @@ if(st.button("Submit")):
 
     if(st.button("Calculate your BMI")):
     
-        height = input_height
-        weight = input_weight
+     height = input_height
+     weight = input_weight
 
-        BMI = (height / weight**2)
-        st.write("Your BMI is ", BMI)
+     BMI = (height / weight**2)
+     st.write(f"Your BMI is {BMI}")
 
-        if BMI <= 18.5:
-            st.write("You are underweight.")
+     if BMI <= 18.5:
+        st.write("You are underweight.")
     
-        elif BMI > 18.5 and BMI <= 24.9:
-            st.write("You have a stable & normal weight.")
+     elif BMI > 18.5 and BMI <= 24.9:
+        st.write("You have a stable & normal weight.")
           
-        elif BMI > 24.9 and BMI <= 29.9:
-            st.write("You are overweight.")
+     elif BMI > 24.9 and BMI <= 29.9:
+        st.write("You are overweight.")
           
-        else:
-            st.write("You are obese.")
+     else:
+        st.write("You are obese.")
           
       
           
