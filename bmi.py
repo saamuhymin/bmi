88ded044c987a80ef2a5bb2532eceb1377389632
saamuhymin <102709114+saamuhymin@users.noreply.github.com>
2022-05-11 12:00:28 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -43,11 +43,36 @@ if(st.button("Submit")):
         st.info("You are underweight.")
         
         if(st.button("What should I do?")):
-            st.text("Don't be picky at whatever you eat. Simply just eat everything.")
+            st.text("Don't be picky at whatever you are served. Simply just eat everything.")
             st.text("Just eat everything but don't be so dumb. You still have to include nutrient-rich foods. Bear that in mind.")
             st.text("Reduce drinking soda or sparkling water. Consume smoothies and shakes that contain calories and protein instead.")
             st.text("Add more toppings for more calories but don't take too much sugar and fat.")
             st.text("Do all of these above and remember to exercise to help you to gain weight by building up muscles.")
+        
+    
+    elif BMI > 18.5 and BMI <= 24.9:
+        st.success("You have a stable & normal weight.")
+        
+        if(st.button('What should I do?')):
+            st.text("Just hang in there! Be consistent and dedicate to what you are doing now.")
+                     
+          
+    elif BMI > 24.9 and BMI <= 29.9:
+        st.warning("You are overweight.")
+        
+        if(st.button('What should I do?')):
+            st.text("A healthy diet. Basically, you should revise the food triangle chart. What should be eaten plenty, some and a few in terms of amount.")
+            st.text("Exercise is a must. You gotta reduce and burn the foods you just ate.")
+            st.text("Always check the calorie information to prevent daily excessive intake.")
+          
+    else:
+        
+        st.error("You are obese.")
+        
+        if(st.button('What should I do?')):
+            st.text("A healthy diet. Basically, you should revise the food triangle chart. What should be eaten plenty, some and a few in terms of amount.")
+            st.text("Exercise is a must. You gotta reduce and burn the excessive fat.")
+            st.text("Get a professional trainer and dietitian to guide you effectively.")
         
     
     
