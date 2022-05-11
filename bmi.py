80c13e6256254b0d0c9e6bb80ef3c9cbc755d4b5
saamuhymin <102709114+saamuhymin@users.noreply.github.com>
2022-05-11 11:41:27 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -43,11 +43,11 @@ if(st.button("Submit")):
         st.info("You are underweight.")
         
         st.button('What should I do?')
-            st.write("Don't be picky at whatever you eat. Simply just eat everything.")
-            st.write("Just eat everything but don't be so dumb. You still have to include nutrient-rich foods. Bear that in mind.")
-            st.write("Reduce drinking soda or sparkling water. Consume smoothies and shakes that contain calories and protein instead.")
-            st.write("Add more toppings for more calories but don't take too much sugar and fat.")
-            st.write("Do all of these above and remember to exercise to help you to gain weight by building up muscles."
+        st.write("Don't be picky at whatever you eat. Simply just eat everything.")
+        st.write("Just eat everything but don't be so dumb. You still have to include nutrient-rich foods. Bear that in mind.")
+        st.write("Reduce drinking soda or sparkling water. Consume smoothies and shakes that contain calories and protein instead.")
+        st.write("Add more toppings for more calories but don't take too much sugar and fat.")
+        st.write("Do all of these above and remember to exercise to help you to gain weight by building up muscles."
         
 
     
@@ -55,25 +55,25 @@ if(st.button("Submit")):
         st.success("You have a stable & normal weight.")
         
         st.button('What should I do?')
-            st.write("Just hang in there! Be consistent and dedicate to what you are doing now.")
+        st.write("Just hang in there! Be consistent and dedicate to what you are doing now.")
                      
           
     elif BMI > 24.9 and BMI <= 29.9:
         st.warning("You are overweight.")
         
         st.button('What should I do?')
-            st.write("A healthy diet. Basically, you should revise the food triangle chart. What should be eaten plenty, some and a few in terms of amount.")
-            st.write("Exercise is a must. You gotta reduce and burn the foods you just ate.")
-            st.write("Always check the calorie information to prevent daily excessive intake.")
+        st.write("A healthy diet. Basically, you should revise the food triangle chart. What should be eaten plenty, some and a few in terms of amount.")
+        st.write("Exercise is a must. You gotta reduce and burn the foods you just ate.")
+        st.write("Always check the calorie information to prevent daily excessive intake.")
           
     else:
         
         st.error("You are obese.")
         
         st.button('What should I do?')
-            st.write("A healthy diet. Basically, you should revise the food triangle chart. What should be eaten plenty, some and a few in terms of amount.")
-            st.write("Exercise is a must. You gotta reduce and burn the excessive fat.")
-            st.write("Get a professional trainer and dietitian to guide you effectively.")
+        st.write("A healthy diet. Basically, you should revise the food triangle chart. What should be eaten plenty, some and a few in terms of amount.")
+        st.write("Exercise is a must. You gotta reduce and burn the excessive fat.")
+        st.write("Get a professional trainer and dietitian to guide you effectively.")
       
           
           
